0694e8154a3e01ba3df95713912357a2ff7d34f8
Oleksiy Boyda <oleksiy.boyda@gmail.com>
2015-10-12 18:22:37 +0200

    Update wp-post-transporter.php

--- wp-post-transporter.php
+++ wp-post-transporter.php
@@ -2,14 +2,12 @@
 /*
 Plugin Name: WP Post Transporter
 Description: This plugin transports posts.
-Version:     0.1
+Version:     1.0
 Author:      Oleksiy Boyda
-License:     Private
+License:     GPL2
 Domain Path: /languages
 Text Domain: wppt
 */
-
-define('WPPT_VERSION', '1.0.0');
 
 add_action('plugins_loaded', 'wppt_load_textomain');
 
@@ -23,8 +21,8 @@ function wppt_enqueue_admin_scripts($hook){
 	if($hook != 'tools_page_wppt-tool'){
 		return;
 	}
-	wp_enqueue_style('wppt-admin-css', plugins_url('/admin.css', __FILE__), array(), WPPT_VERSION);
-	wp_enqueue_script('wppt-admin-js', plugins_url('/admin.js', __FILE__), array('jquery'), WPPT_VERSION);
+	wp_enqueue_style('wppt-admin-css', plugins_url('/admin.css', __FILE__), array());
+	wp_enqueue_script('wppt-admin-js', plugins_url('/admin.js', __FILE__), array('jquery'));
 }
 
 require(dirname(__FILE__) . '/helpers.php');
